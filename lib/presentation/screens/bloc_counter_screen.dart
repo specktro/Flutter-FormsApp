e352641944bd4a459d41641f320a0d42607c4573
specktro <specktro@nonull.mx>
2025-06-30 23:21:58 -0600

    feat: Implement go_router and basic app structure
    This commit introduces the `go_router` package for navigation and sets up the basic application structure.
    
    Changes include:
    - Added `go_router` dependency to `pubspec.yaml`.
    - Configured `appRouter` with initial routes for `HomeScreen` and `CubitCounterScreen`.
    - Updated `main.dart` to use `MaterialApp.router` with the configured `appRouter` and a new `AppTheme`.
    - Created `HomeScreen` with a `ListView` to navigate to different sections.
    - Created placeholder screens: `CubitCounterScreen` and `BlocCounterScreen`.
    - Added an `AppTheme` class for basic theme configuration.
    - Created a `screens.dart` barrel file for easier import of screen widgets.

---
+++ lib/presentation/screens/bloc_counter_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class BlocCounterScreen extends StatelessWidget {
+  const BlocCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bloc Counter'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.refresh_rounded))
+        ],
+      ),
+      body: Center(
+        child: const Text('Counter value: xxxx'),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: '1',
+            child: const Text('+3'),
+            onPressed: () {},
+          ),
+          SizedBox(height: 15),
+          FloatingActionButton(
+            heroTag: '2',
+            child: const Text('+2'),
+            onPressed: () {},
+          ),
+          SizedBox(height: 15),
+          FloatingActionButton(
+            heroTag: '3',
+            child: const Text('+1'),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
